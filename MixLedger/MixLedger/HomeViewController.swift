cda2948e957810359e913b65cb2b0d0073cf3759
puma <cwenyu2424@gmail.com>
2023-11-15 15:18:23 +0800

    [docs] add project file & .gitignore

---
+++ MixLedger/MixLedger/HomeViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MixLedger
+//
+//  Created by 莊羚羊 on 2023/11/14.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
